8efaf82f0520135cdee162c5488339138af8d6b3
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-19 18:55:20 -0500

    The way teach does it

--- ToDo.java
+++ ToDo.java
@@ -1,25 +1,26 @@
 //ToDo def
-import java.util.Scanner;
 
+package todo;
 public class ToDo {
-	Scanner inputDevice = new Scanner(System.in);
-	public static int ToDoAmount = 0;
-	boolean Done = false;
-	String WhatToDo;
 	
-	public void addToDo(){
-		ToDoAmount++;
-		
+	public static int ToDoAmount = 0;
+	
+	private boolean isDone = false;
+	private String WhatToDo;
+	
+	public void addToDo(String i){
+		ToDoAmount=i;
+		count ++;
 	}
 	public void markDone(){
-		Done=true;
+		isDone=true;
 	}
-	public void GetCount(){
-		System.out.println("The amount of to do's" + ToDoAmount);
+	public int GetCount(){
+		return ToDoAmount;
 	}
-	public void GetToDo()
+	public String GetToDo()
 	{
-		WhatToDo=inputDevice.nextLine();
-		System.out.println("What is the to do" + WhatToDo);
+		
+		return WhatToDo;
 	}
 }
