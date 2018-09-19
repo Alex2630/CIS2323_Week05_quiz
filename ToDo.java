d2df27003a5a6a8a2d598b64538a38033095852c
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-19 18:47:01 -0500

    Initial commit

---
+++ ToDo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ToDo {
+	Scanner inputDevice = new Scanner(System.in);
+	public static int ToDoAmount = 0;
+	boolean Done = false;
+	String WhatToDo;
+	
+	public void addToDo(){
+		ToDoAmount++;
+		
+	}
+	public void markDone(){
+		Done=true;
+	}
+	public void GetCount(){
+		System.out.println("The amount of to do's" + ToDoAmount);
+	}
+	public void GetToDo()
+	{
+		WhatToDo=inputDevice.nextLine();
+		System.out.println("What is the to do" + WhatToDo);
+	}
+}
---
+++ ToDoList.java
@@ -0,0 +1,19 @@
+public class ToDoList{
+	public static void main ( String[] args){
+			ToDo myToDo= new ToDo();
+			AddToDo();
+			markDone();
+			Getcount();
+			GetToDo();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+}
